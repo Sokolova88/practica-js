8d9f80490e3a116d8c86429009041ac7f69b63e4
Sokolova88 <[sokolova.studies@gmail.com]>
2024-10-02 11:34:26 +0300

    Task by dom - example

---
+++ js/dom.js
@@ -0,0 +1,21 @@
+{
+  /* <a class="link is-active" href="">A random link</a> */
+}
+
+const link = document.querySelector(".link");
+console.log(link.classList);
+
+const hasActiveClass = link.classList.contains("is-active");
+console.log(`hasActiveClass - ${hasActiveClass}`);
+
+link.classList.add("special");
+console.log(link.classList);
+
+link.classList.remove("is-active");
+console.log(link.classList);
+
+link.classList.toggle("is-active");
+console.log(link.classList);
+
+link.classList.replace("special", "regular");
+console.log(link.classList);
